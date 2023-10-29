ffdb0eab83075bea962cef5a78aa996c7243cc1a
Bernardo de Araujo <bernardo.araujo@shopify.com>
2023-10-29 12:02:49 -0400

    Add questions for Verb "ser" in seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -13,32 +13,36 @@ topics = %{
   "verb_estar.csv" => %{
     name: "Estar - Presente",
     description: "Questions related to the verb \"estar\", which is the verb \"to be\" for temporary situations."
+  },
+  "verb_ser.csv" => %{
+    name: "Ser - Presente",
+    description: "Questions related to the verb \"ser\", which is the verb \"to be\" for permanent situations."
   }
 }
 
-Enum.each(topics, fn {filename, %{name: name, description: description} = topic_attrs} ->
+Enum.each(topics, fn {filename, %{name: name} = topic_attrs} ->
   slug = Slugy.slugify(name)
 
-  topic = case Repo.get_by(Topic, slug: slug) do
+  case Repo.get_by(Topic, slug: slug) do
     nil ->
       Logger.info("Creating topic: #{name}")
-      %Topic{} |> Ecto.Changeset.change(Map.put_new(topic_attrs, :slug, slug)) |> Repo.insert!()
+      topic = %Topic{} |> Ecto.Changeset.change(Map.put_new(topic_attrs, :slug, slug)) |> Repo.insert!()
+
+      "./datasets/#{filename}"
+      |> Path.expand(__DIR__)
+      |> File.stream!()
+      |> CSV.decode(separator: ?|, headers: false)
+      |> Enum.each(fn {:ok, [problem, answer]} ->
+        changeset = Question.changeset(%Question{}, %{topic_id: topic.id, problem: problem, answer: answer})
+        case Repo.insert(changeset) do
+          {:ok, _record} ->
+            Logger.info("Entry inserted: #{problem} | #{answer}")
+          {:error, changeset} ->
+            Logger.error("Failed to insert entry: #{problem} | #{answer}. Errors: #{inspect(changeset.errors)}")
+        end
+      end)
 
     topic ->
-      topic
+      Logger.info("Topics already exists: #{topic.name}")
   end
-
-  "./datasets/#{filename}"
-  |> Path.expand(__DIR__)
-  |> File.stream!()
-  |> CSV.decode(separator: ?|, headers: false)
-  |> Enum.each(fn {:ok, [problem, answer]} ->
-    changeset = Question.changeset(%Question{}, %{topic_id: topic.id, problem: problem, answer: answer})
-    case Repo.insert(changeset) do
-      {:ok, _record} ->
-        Logger.info("Entry inserted: #{problem} | #{answer}")
-      {:error, changeset} ->
-        Logger.error("Failed to insert entry: #{problem} | #{answer}. Errors: #{inspect(changeset.errors)}")
-    end
-  end)
 end)
